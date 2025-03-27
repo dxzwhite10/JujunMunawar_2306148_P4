434e3e41551c86b37b5f3da73474a286a3685c6a
zumar <munawarjujun7@gmail.com>
2025-03-27 11:29:08 +0700

    Add files via upload

---
+++ pernikahan.pl
@@ -0,0 +1,14 @@
+anakIbu(andi).
+anakIbu(budi).
+anakIbu(cika).
+anakIbu(dani).
+anakIbu(emil).
+not(anakIbu(fadil)).
+
+sukaPermen(andi).
+sukaPermen(budi).
+sukaPermen(cika).
+not(sukaPermen(danil)).
+not(sukaPermen(emil)).
+
+
